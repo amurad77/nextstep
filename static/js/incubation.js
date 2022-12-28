ca502242e2c419182c2d8ced418982c8ad6c9721
sheyda99 <sheydasultan.99@gmail.com>
2022-12-28 22:37:37 +0400

    fixing

--- static/js/incubation.js
+++ static/js/incubation.js
@@ -2,7 +2,7 @@ showMore = document.getElementById("show-more");
 moreTrainers = document.getElementById("show-more-trainers");
 
 showMore.addEventListener("click", function(e) {
-    moreTrainers.style.display = "block";
+    moreTrainers.style.display = "flex";
     showMore.style.display = "none";
 
     e.preventDefault();
