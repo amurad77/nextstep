fd54355d8116d20df7f35149ac95791cf28d59f9
amurad77 <amurad7799@gmail.com>
2022-12-28 21:20:20 +0400

    add static

--- nextstep/settings.py
+++ nextstep/settings.py
@@ -141,16 +141,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static"
-# ]
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+# if PROD:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "./staticfiles/")
+# else:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, "./staticfiles/")
+#     ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
