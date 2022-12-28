69e5d94dbd67421d0b6f3cbeef3da9f5cbb666f3
amurad77 <amurad7799@gmail.com>
2022-12-28 21:16:22 +0400

    add static

--- nextstep/settings.py
+++ nextstep/settings.py
@@ -145,10 +145,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
